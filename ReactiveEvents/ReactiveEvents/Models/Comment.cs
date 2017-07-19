08149297a8826d3ba36516b68e4805b4299d5d3f
Mihaela Rotariu <rotariumihaela.bv@gmail.com>
2017-07-19 23:27:54 +0300

    React_7.19.2017

---
+++ ReactiveEvents/ReactiveEvents/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactiveEvents.Models
+{
+    public class Comment
+    {
+
+
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+        public string Text { get; set; }
+
+    }
+}
